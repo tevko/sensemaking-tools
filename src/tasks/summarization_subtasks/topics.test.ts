90c4597fc8ca41923bf846152ef1c12b81e6312c
Jigsaw <no-reply@google.com>
2025-01-17 09:41:20 -0800

    Internal change
    GitOrigin-RevId: 5a4a1ab13e86c42594a7d08126851bc48edbeedd

---
+++ src/tasks/summarization_subtasks/topics.test.ts
@@ -0,0 +1,84 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { VertexModel } from "../../models/vertex_model";
+import { SummaryStats } from "../../stats_util";
+import { Comment } from "../../types";
+import { TopicsSummary } from "./topics";
+
+const TEST_COMMENTS: Comment[] = [
+  {
+    id: "1",
+    text: "comment 1",
+    topics: [{ name: "Topic A", subtopics: [{ name: "Subtopic A.1" }] }],
+  },
+  {
+    id: "2",
+    text: "comment 2",
+    topics: [{ name: "Topic A", subtopics: [{ name: "Subtopic A.1" }] }],
+  },
+  {
+    id: "3",
+    text: "comment 3",
+    topics: [{ name: "Topic A", subtopics: [{ name: "Subtopic A.2" }] }],
+  },
+  {
+    id: "4",
+    text: "comment 4",
+    topics: [{ name: "Topic B", subtopics: [{ name: "Subtopic B.1" }] }],
+  },
+];
+
+describe("TopicsSummaryTest", () => {
+  it("should create a properly formatted topics summary", async () => {
+    expect(
+      await new TopicsSummary(
+        new SummaryStats(TEST_COMMENTS),
+        new VertexModel("project123", "usa")
+      ).getSummary()
+    ).toEqual(`## Topics
+
+From the comments submitted, 2 high level topics were identified, as well as 3 subtopics. Based on voting patterns between the opinion groups described above, both points of common ground as well as differences of opinion between the groups have been identified and are described below.
+
+### Topic A (3 comments)
+
+This topic included 2 subtopics.
+
+#### Subtopic A.1 (2 comments)
+
+Common ground: Some points of common ground...
+
+Differences of opinion: Areas of disagreement between groups...
+
+#### Subtopic A.2 (1 comments)
+
+Common ground: Some points of common ground...
+
+Differences of opinion: Areas of disagreement between groups...
+
+
+### Topic B (1 comments)
+
+This topic included 1 subtopic.
+
+#### Subtopic B.1 (1 comments)
+
+Common ground: Some points of common ground...
+
+Differences of opinion: Areas of disagreement between groups...
+
+
+`);
+  });
+});
--- src/tasks/summarization_subtasks/topics.ts
+++ src/tasks/summarization_subtasks/topics.ts
@@ -15,14 +15,90 @@
 // Functions for different ways to summarize Comment and Vote data.
 
 import { RecursiveSummary } from "./recursive_summarization";
+import { TopicStats } from "../../stats_util";
 
 export class TopicsSummary extends RecursiveSummary {
-  getSummary() {
-    // return a promise that resolves to a fixed constrant string
+  async getSummary() {
+    // First construct the introductory description for the entire section
+    const topicStats: TopicStats[] = this.input.getStatsByTopic();
+    const nTopics: number = topicStats.length;
+    const nSubtopics: number = topicStats
+      .map((t) => t.subtopicStats?.length || 0)
+      .reduce((n, m) => n + m, 0);
+    const hasSubtopics: boolean = nSubtopics > 0;
+    const subtopicsCountText: string = hasSubtopics ? `, as well as ${nSubtopics} subtopics` : "";
+    const overviewText: string =
+      `From the comments submitted, ${nTopics} high level topics were identified${subtopicsCountText}. ` +
+      `Based on voting patterns between the opinion groups described above, both points of common ground as well as differences of opinion between the groups have been identified and are described below.`;
+
+    // Now construct the individual Topic summaries
+    const topicSummaries: Array<Promise<string>> = topicStats.map((topicStat) =>
+      this.getTopicSummary(topicStat)
+    );
+    const topicSummaryText: string = await Promise.all(topicSummaries).then((summaries) =>
+      summaries.join("\n")
+    );
+
     return Promise.resolve(
       `## Topics
 
-This is a mock topic summary.`
+${overviewText}
+
+${topicSummaryText}
+`
+    );
+  }
+
+  /**
+   * Generate a summary for the given topic.
+   * @param subtopicStat A TopicStats value, representing a particular top level topic.
+   * @returns A summary of the given topic.
+   */
+  async getTopicSummary(topicStat: TopicStats): Promise<string> {
+    const sectionTitle: string = `${topicStat.name} (${topicStat.commentCount} comments)`;
+    const subtopicSummaries: Array<Promise<string>> =
+      topicStat.subtopicStats?.map((subtopicStat) => this.getSubtopicSummary(subtopicStat)) || [];
+    const nSubtopics: number = topicStat.subtopicStats?.length || 0;
+    const subtopicsSummaryText: string =
+      subtopicSummaries.length > 0
+        ? await Promise.all(subtopicSummaries).then((summaries) => summaries.join("\n"))
+        : "";
+    // This is just a stub for now, and may eventually be added on to include more naunced descriptions of e.g. where the highest
+    // points of common ground and most significant differences of opinion were across the subtopics.
+    const topicSummary =
+      nSubtopics > 0
+        ? `This topic included ${nSubtopics} subtopic${nSubtopics === 1 ? "" : "s"}.`
+        : "";
+
+    return Promise.resolve(
+      `### ${sectionTitle}
+
+${topicSummary}
+
+${subtopicsSummaryText}
+`
+    );
+  }
+
+  /**
+   * Generate a summary for the given subtopic.
+   * @param subtopicStat A TopicStats value, representing a particular subtopic.
+   * @returns A summary of the given subtopic.
+   */
+  async getSubtopicSummary(subtopicStat: TopicStats): Promise<string> {
+    const sectionTitle: string = `${subtopicStat.name} (${subtopicStat.commentCount} comments)`;
+
+    // For now, these are mocked out...
+    const commonGroundSummary = "Some points of common ground...";
+    const differencesSummary = "Areas of disagreement between groups...";
+
+    return Promise.resolve(
+      `#### ${sectionTitle}
+
+Common ground: ${commonGroundSummary}
+
+Differences of opinion: ${differencesSummary}
+`
     );
   }
 }
